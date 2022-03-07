84fc7427090360724dd0a8db54a49bf8f5f21cc7
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-03-07 15:58:11 +0100

    finish es 4

--- src/test/scala/u02lab/CurrierExTest.scala
+++ src/test/scala/u02lab/CurrierExTest.scala
@@ -12,3 +12,12 @@ class CurrierExTest:
   @Test def testP2() =
     assertTrue(p2(5,10,15));
     assertFalse(p2(20,10,15));
+
+  @Test def testP3() =
+    assertTrue(p3(5)(10)(15));
+    assertFalse(p3(20)(10)(15));
+    
+  @Test def testP4()=
+    assertTrue(p4(5,10,15));
+    assertFalse(p4(20,10,15));
+    
