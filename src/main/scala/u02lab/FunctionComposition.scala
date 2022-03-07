221506a59621490489d22e350421c6a52798b631
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-03-07 19:37:36 +0100

    implement function composition

---
+++ src/main/scala/u02lab/FunctionComposition.scala
@@ -0,0 +1,8 @@
+package u02lab
+
+object FunctionComposition:
+  object FunctionComposition:
+    val compose: (f: Int => Int, g: Int => Int)=> Int => Int = (f,g) => i => f(g(i));
+
+    def genericCompose[T, X, Y]: (f: X => Y, g: T => X)=> T => Y = (f,g) => i => f(g(i));
+
