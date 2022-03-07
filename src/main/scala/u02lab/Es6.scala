bab9d0a72856a148d535e5ea9405476304c7e7e3
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-03-07 22:24:09 +0100

    implement ex 6 about fibonacci

--- src/main/scala/u02lab/Es6.scala
+++ src/main/scala/u02lab/Es6.scala
@@ -2,4 +2,15 @@ package u02lab
 
 object Es6 extends App:
   object Fibonacci:
-    
+    def fib (n: Int): Int = n match
+      case 0 | 1 => n
+      case _ => fib(n-1) + fib(n-2);
+
+
+    def tailFib(n: Int): Int =
+      @annotation.tailrec
+      def _tailFib (n: Int, prevSum: Int, sum: Int): Int = n match
+        case 0 => prev
+        case 1 => sum
+        case _ => _tailFib(n-1, sum , sum + prevSum)
+      _tailFib(n, 0, 1)
--- src/test/scala/u02lab/FibonacciTest.scala
+++ src/test/scala/u02lab/FibonacciTest.scala
@@ -1,7 +1,19 @@
 package u02lab
 import org.junit.*
 import org.junit.Assert.*
+import Es6.Fibonacci.*
 
 class FibonacciTest:
-  @Test def testFib()=
-    assertEquals(true, true);
+  @Test def testFibonacci()=
+    assertEquals(0, fib(0));
+    assertEquals(1, fib(1));
+    assertEquals(1, fib(2));
+    assertEquals(2, fib(3));
+    assertEquals(3, fib(4));
+
+  @Test def testFibonacciTail() =
+    assertEquals(0, tailFib(0));
+    assertEquals(1, tailFib(1));
+    assertEquals(1, tailFib(2));
+    assertEquals(2, tailFib(3));
+    assertEquals(3, tailFib(4));
