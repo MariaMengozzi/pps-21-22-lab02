92629ae3f51a788d89f30de8f58bf4fa8233291d
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-03-11 21:07:00 +0100

    professor correction

--- src/main/scala/u02lab/Es5.scala
+++ src/main/scala/u02lab/Es5.scala
@@ -4,5 +4,5 @@ object Es5:
   object FunctionComposition:
     val compose: (f: Int => Int, g: Int => Int)=> Int => Int = (f,g) => i => f(g(i));
 
-    def genericCompose[T, X, Y]: (f: X => Y, g: T => X)=> T => Y = (f,g) => i => f(g(i));
+    def genericCompose[T, X, Y] (f: X => Y, g: T => X): T => Y = i => f(g(i));
 
--- src/test/scala/u02lab/BTreeTest.scala
+++ src/test/scala/u02lab/BTreeTest.scala
@@ -7,22 +7,22 @@ import u02lab.Es9.Tree.*
 class BTreeTest:
   val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(1))
   @Test def testBTreeGeneralisationSize(): Unit=
-    val p: (Int, Int) => Int = (x,y) => x+y;
+    val p: (Int, Int) => Int = (x,y) => _+_;
     val pL: (Int) => Int = _ => 1
     assertEquals(3, generalisation (tree, p, pL))
 
   @Test def testBTreeGeneralisationFind(): Unit=
-    val p: (Boolean, Boolean) => Boolean = (x,y) => x || y;
+    val p: (Boolean, Boolean) => Boolean = (x,y) => _ || _;
     val pL: (Int) => Boolean = e => e == 1
     assertTrue(generalisation (tree, p, pL))
 
   @Test def testBTreeGeneralisationFind2(): Unit=
-    val p: (Boolean, Boolean) => Boolean = (x,y) => x || y;
+    val p: (Boolean, Boolean) => Boolean = (x,y) => _ || _;
     val pL: (Int) => Boolean = e => e == 4
     assertFalse(generalisation (tree,p, pL))
 
   @Test def testBTreeGeneralisationCount(): Unit=
-    val p: (Int, Int) => Int = (x,y) => x+y;
+    val p: (Int, Int) => Int = (x,y) => _+_;
     val pL: (Int) => Int = e => e match
       case x if (x == 1) => 1
       case _ => 0
